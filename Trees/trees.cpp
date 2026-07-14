88d2512ebfe9fe09ced10947e0bd599d541bfacc
Nirajan Acharya <nirajanacharya@kec.edu.np>
2026-07-14 06:40:45 +0000

    add

---
+++ Trees/trees.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+// Node Structure
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+};
+
+// Function to Create a New Node
+Node* createNode(int data)
+{
+    Node *newNode = new Node();
+
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+// Inorder Traversal
+void inorder(Node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+// Preorder Traversal
+void preorder(Node *root)
+{
+    if (root != NULL)
+    {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+// Postorder Traversal
+void postorder(Node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+int main()
+{
+    // Create Nodes
+    Node *root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+
+    cout << "Inorder: ";
+    inorder(root);
+
+    cout << "\nPreorder: ";
+    preorder(root);
+
+    cout << "\nPostorder: ";
+    postorder(root);
+
+    return 0;
+}
